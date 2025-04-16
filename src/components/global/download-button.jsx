83a308ffc4859e754bfd1ae2c18895868983d1d4
Alejandro Zepeda <guerozepeda02@gmail.com>
2025-04-16 15:49:36 -0700

    Fixing Link

--- src/components/global/download-button.jsx
+++ src/components/global/download-button.jsx
@@ -2,7 +2,7 @@ import { IoMdDownload } from "react-icons/io"
 export const DownloadButton = ({link}) =>{
 
     return(
-        <a href={`link`} target="_blank">
+        <a href={`${link}`} target="_blank">
             <button className="btn btn-primary"><IoMdDownload />Download</button>
         </a>
     )
