0455376c04eedc228f46233e8c51026b7bda54ce
Alejandro Zepeda <guerozepeda02@gmail.com>
2025-04-16 15:48:23 -0700

    Removing Endpoint

--- src/components/global/download-button.jsx
+++ src/components/global/download-button.jsx
@@ -1,9 +1,8 @@
 import { IoMdDownload } from "react-icons/io"
-import {ENDPOINT} from "../../util/constants"
 export const DownloadButton = ({link}) =>{
 
     return(
-        <a href={`${ENDPOINT}${link}`} target="_blank">
+        <a href={`link`} target="_blank">
             <button className="btn btn-primary"><IoMdDownload />Download</button>
         </a>
     )
