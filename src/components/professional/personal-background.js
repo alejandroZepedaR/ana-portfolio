8fccee3a72fe535cd5066863b0cc82ffaa7ca35a
Alejandro Zepeda <guerozepeda02@gmail.com>
2025-04-16 15:53:09 -0700

    Adding Personal Background

--- src/components/professional/personal-background.js
+++ src/components/professional/personal-background.js
@@ -5,10 +5,11 @@ export const PersonalBackground = () => {
             <header>
                 <h2 className="text-2xl">Personal Background</h2>
             </header>
-            <main>
-                <p className="text-md">
-                    here goes a brief personal background: introduce yourself, your personal background, and your
-                    personal goals.
+            <main className="prose md:prose-lg">
+                <p>
+                    As a proud Hispanic woman from Mexico, my bicultural background has greatly influenced who I am now. I've had the unique opportunity to learn about the difficulties and advantages of both cultures as a result of growing up in Mexico and then adapting to life in the US. My early exposure to the reality of systematic injustice, corruption, and inequality while living in Mexico fueled my passion for social justice and my lifelong dedication to helping marginalized communities. Fluent in both English and Spanish, I use my language skills to bridge gaps and build trust with individuals from diverse backgrounds, especially those who face barriers to accessing the support and resources they need. 
+                    My own experience has stoked a strong sense of empathy and determination. I'm dedicated to fighting for just, inclusive systems that elevate rather than marginalize people and amplifying the voices of those who are frequently disregarded. I try to have a real influence on people's lives, whether it be through ordinary acts of kindness or my work with refugees at the International Rescue Committee.
+                    My identity is not just a part of who I am—it’s the foundation of my purpose. I believe in the power of community, the importance of representation, and the urgency of social change. These principles still motivate me as I evolve professionally and personally, and I'm committed to making the world more fair and just for everybody. 
                 </p>
             </main>
         </section>
